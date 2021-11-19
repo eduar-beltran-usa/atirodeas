12dcb9a91b6dd14564156e032bdec8f60b41441f
aplicacionhibrida <eduard-beltranb@unilibre.edu.co>
2021-11-19 11:20:51 -0500

    first commit

---
+++ src/main/java/com/atirodeas/AtirodeasApplication.java
@@ -0,0 +1,13 @@
+package com.atirodeas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtirodeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtirodeasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atirodeas/AtirodeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atirodeas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtirodeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
